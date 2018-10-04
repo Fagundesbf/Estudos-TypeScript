8f35b4f40ea1ff7ec39fd87a013939eb2a85124f
Fagundesbf <43768001+Fagundesbf@users.noreply.github.com>
2018-10-04 02:13:00 -0300

    AULA 9  funcoes

--- dist/dist/app.js
+++ dist/dist/app.js
@@ -7,10 +7,21 @@ console.log("Proximo episode é " + episode);
 var favoriteDroid;
 favoriteDroid = 'BB-8';
 console.log("Meu Android Favorito é " + favoriteDroid);
-// AULA USANDO FUNÇÕES 
+//9- AULA 
+//USANDO FUNÇÕES 
 var isEnoughToBeatMF = function (parsecs) {
     return parsecs < 12;
 };
-var distance = 14;
-// TEMPLETE STRING ACTION SCRIPT, NAO SE USA"" E SIM A CRAZE `   ` DOLLAR E CHAVE 
+var distance = 11;
 console.log("Is " + distance + " parsecs enough to beat Millennium Falcon ? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy," + name + "?"); };
+call('R2');
+//PARAMETROS PADROES 
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+//MODELO 1
+console.log("inc (5,1) = " + inc(5, 1));
+//MODELO 2
+console.log("inc (5) = " + inc(5));
